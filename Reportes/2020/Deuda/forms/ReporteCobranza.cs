8f126fbff3c18bb69d6d399d90a781d227fac675
Maick Davila <librajunior13@gmail.com>
2021-01-08 16:17:22 -0500

    Autogeneratet20211817328

--- Reportes/2020/Deuda/forms/ReporteCobranza.cs
+++ Reportes/2020/Deuda/forms/ReporteCobranza.cs
@@ -44,6 +44,14 @@ namespace Presentacion.Reportes._2020.Deuda.forms
                 reportViewer1.LocalReport.DataSources.Clear();
                 reportViewer1.LocalReport.EnableExternalImages = true;
                 ParametrosReporte("DataSet1", (DataTable)tabla, @"2020\Deuda\ReporteCobranza.rdlc", reportViewer1);
+                //
+                datasets.DataSetReporteEmitidosTableAdapters.sp_reporte_comprobantes_emitidosTableAdapter ta2 = new datasets.DataSetReporteEmitidosTableAdapters.sp_reporte_comprobantes_emitidosTableAdapter();
+                ta2.Connection = new System.Data.SqlClient.SqlConnection(DataSetConexion);
+
+                datasets.DataSetReporteEmitidos.sp_reporte_comprobantes_emitidosDataTable tabla2 = new datasets.DataSetReporteEmitidos.sp_reporte_comprobantes_emitidosDataTable();
+                ta2.Fill(tabla2, FechaIni, FechaFin);
+                ParametrosReporte("DataSet2", (DataTable)tabla2, @"2020\Deuda\ReporteCobranza.rdlc", reportViewer1);
+
                 this.reportViewer1.RefreshReport();
             }
             catch (Exception ex)
