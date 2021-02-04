6faa77307c4c2e51fc1291cf977f464d804ad33c
Maick Davila <librajunior13@gmail.com>
2021-02-04 15:34:27 -0500

    Autogeneratet20212434233

---
+++ Reportes/2020/AlmacenMovimiento/DataSet/DataSet_sp_get_reporte_cobranza_vendedor_almacenMovimiento.cs
@@ -0,0 +1,15 @@
+﻿namespace Presentacion.Reportes._2020.AlmacenMovimiento.DataSet
+{
+
+
+    partial class DataSet_sp_get_reporte_cobranza_vendedor_almacenMovimiento
+    {
+    }
+}
+
+namespace Presentacion.DataSet_sp_get_reporte_cobranza_vendedor_almacenMovimientoTableAdapters {
+    
+    
+    public partial class sp_get_reporte_cobranza_vendedor_almacenMovimientoTableAdapter {
+    }
+}
---
+++ Reportes/2020/AlmacenMovimiento/forms/Form_sp_get_reporte_cobranza_vendedor_almacenMovimiento.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Presentacion.Reportes._2020.AlmacenMovimiento.forms
+{
+    public partial class Form_sp_get_reporte_cobranza_vendedor_almacenMovimiento : Imprimir
+    {
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaFin { get; set; }
+        public int IdVendedor { get; set; }
+
+        public Form_sp_get_reporte_cobranza_vendedor_almacenMovimiento()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_sp_get_reporte_cobranza_vendedor_almacenMovimiento_Load(object sender, EventArgs e)
+        {
+            Imprimir();
+        }
+
+        void Imprimir()
+        {
+            try
+            {
+                LLenar_2();
+
+                AlmacenMovimiento.DataSet.DataSet_sp_get_reporte_cobranza_vendedor_almacenMovimientoTableAdapters.sp_get_reporte_cobranza_vendedor_almacenMovimientoTableAdapter ta = new DataSet.DataSet_sp_get_reporte_cobranza_vendedor_almacenMovimientoTableAdapters.sp_get_reporte_cobranza_vendedor_almacenMovimientoTableAdapter();
+                ta.Connection = new System.Data.SqlClient.SqlConnection(DataSetConexion);
+
+                AlmacenMovimiento.DataSet.DataSet_sp_get_reporte_cobranza_vendedor_almacenMovimiento.sp_get_reporte_cobranza_vendedor_almacenMovimientoDataTable tabla = new DataSet.DataSet_sp_get_reporte_cobranza_vendedor_almacenMovimiento.sp_get_reporte_cobranza_vendedor_almacenMovimientoDataTable();
+                ta.Fill(tabla, FechaInicio, FechaFin, IdVendedor);
+                reportViewer1.LocalReport.DataSources.Clear();
+                reportViewer1.LocalReport.EnableExternalImages = true;
+                ParametrosReporte("DataSet1", (DataTable)tabla, "2020//AlmacenMovimiento//get_reporte_cobranza_vendedor_almacenMovimiento.rdlc", reportViewer1);
+                this.reportViewer1.RefreshReport();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
